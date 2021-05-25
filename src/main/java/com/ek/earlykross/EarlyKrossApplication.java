53c72c0da87d01fc60cab9bc483008abd5ed4bf6
jinyoung <jyson19@gmail.com>
2021-05-25 22:49:40 +0900

    [add] project init

---
+++ src/main/java/com/ek/earlykross/EarlyKrossApplication.java
@@ -0,0 +1,13 @@
+package com.ek.earlykross;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarlyKrossApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EarlyKrossApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ek/earlykross/EarlyKrossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ek.earlykross;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarlyKrossApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
