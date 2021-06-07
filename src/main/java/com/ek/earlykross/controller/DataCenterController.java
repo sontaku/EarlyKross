4de0f6d5adc2c8aea2f328b6ca30759e82d33ef2
jyson19 <jyson19@gmail.com>
2021-06-07 11:14:53 +0900

    [mod] 데이터센터 카테고리 변경

--- src/main/java/com/ek/earlykross/controller/DataCenterController.java
+++ src/main/java/com/ek/earlykross/controller/DataCenterController.java
@@ -1,29 +1,13 @@
 package com.ek.earlykross.controller;
 
 import com.ek.earlykross.repository.ClubRepository;
-import com.ek.earlykross.service.DataCenterService;
-import com.ek.earlykross.vo.MemoDTO;
-import com.ek.earlykross.vo.PageRequestDTO;
-import com.ek.earlykross.vo.SampleVO;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import lombok.extern.log4j.Log4j2;
 
 
 @Controller
@@ -50,14 +34,16 @@ public class DataCenterController {
 //    }
 
     // 리그 정보 페이지
-    @GetMapping({"/league_overview.do"})
+    @GetMapping({"/league.do"})
     public void leagueOverview(Model model) {
-        log.info("dcController.league_overview 호출");
+        log.info("DataCenterController.LeagueOverview 호출");
+//        return "redirect:/league/overview.do";
     }
 
     //
-    @GetMapping({"/team_overview.do"})
-    public void teamOverview(Model model) {
-        log.info("dcController.team_overview 호출");
+    @GetMapping({"/club.do"})
+    public void clubOverview(Model model) {
+        log.info("DataCenterController.ClubOverview 호출");
+//        return "redirect:/club/overview.do";
     }
 }
