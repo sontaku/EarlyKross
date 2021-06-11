13debff0d9de8307e6656cb560dca927bfeb2b88
Taeyoung Park <typ0307@gmail.com>
2021-06-11 15:26:11 +0900

    [add]redis 적용

---
+++ src/main/java/com/ek/earlykross/controller/ChatController.java
@@ -0,0 +1,15 @@
+package com.ek.earlykross.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/chat")
+public class ChatController {
+
+  @GetMapping("/chat.do")
+  public void chat() {
+
+  }
+}
