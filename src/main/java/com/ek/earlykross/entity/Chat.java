084b35c143d273ab71368488d5dd41ef6851daff
Taeyoung Park <typ0307@gmail.com>
2021-06-13 22:51:04 +0900

    [add] chat 엔티티 추가 및 chat 용 파일 추가

---
+++ src/main/java/com/ek/earlykross/entity/Chat.java
@@ -0,0 +1,42 @@
+package com.ek.earlykross.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+// 채팅 정보
+
+@Entity // 엔티티를 위한 클래스(클래스 내 인스턴스를 JPA로 관리)
+@Table(name = "chat") // name으로 지정한 이름으로 테이블 생성
+@ToString
+@Getter
+@Builder // 객체 생성
+@AllArgsConstructor
+@NoArgsConstructor
+public class Chat {
+
+  @Id // PK 지정
+  private Long cId; // 채팅번호
+
+  @Column
+  private String rId; // 방번호
+
+  @ManyToOne
+  @JoinColumn(name = "mId", nullable = false)
+  private Member mId; // 멤버 ID
+
+  @Column(nullable = false)
+  private String cMsg; // 채팅 내용
+
+  @Column(nullable = false)
+  private String cDate; // 채팅 날짜
+
+}
---
+++ src/main/resources/templates/chat/server.js
@@ -0,0 +1,80 @@
+var app = require("express")();
+var server = require("http").createServer(app);
+// http server를 socket.io server로 upgrade한다
+var io = require("socket.io")(server);
+
+const mysql = require("mysql"); // mysql 모듈 로드
+const conn = {
+  // mysql 접속 설정
+  host: "localhost",
+  port: "3306",
+  user: "ek",
+  password: "ek",
+  database: "earlykross",
+};
+
+fId = "";
+// localhost:3000으로 서버에 접속하면 클라이언트로 index.html을 전송한다
+app.get("/", function (req, res) {
+  res.sendFile("C:\\Users\\Siat\\IdeaProjects\\EarlyKross\\src\\main\\resources\\templates\\chat\\chat.html");
+  // res.sendFile(__dirname + "/index-room.html");
+  fId = req.query["fId"];
+  console.log(fId);
+});
+
+// namespace /chat에 접속한다.
+var chat = io.of("/chat").on("connection", function (socket) {
+  socket.on("chat message", function (data) {
+    console.log("message from client: ", data);
+
+    var connection = mysql.createConnection(conn); // DB 커넥션 생성
+    connection.connect(); // DB 접속
+
+    var sql =
+      "INSERT INTO chat(room, name, msg) VALUES(" +
+      // connection.escape(data.room) +
+      connection.escape(fId) +
+      "," +
+      connection.escape(data.name) +
+      "," +
+      connection.escape(data.msg) +
+      ")";
+
+    connection.query(sql, function (err, results, fields) {
+      // testQuery 실행
+      if (err) {
+        console.log(err);
+      }
+      console.log(results);
+    });
+
+    // testQuery = "SELECT * FROM chat";
+
+    // connection.query(testQuery, function (err, results, fields) {
+    //   // testQuery 실행
+    //   if (err) {
+    //     console.log(err);
+    //   }
+    //   console.log(results);
+    // });
+
+    connection.end(); // DB 접속 종료
+
+    var name = (socket.name = data.name);
+    var room = fId;
+
+    console.log(room);
+
+    // room에 join한다
+    socket.join(room);
+    // room에 join되어 있는 클라이언트에게 메시지를 전송한다
+    // fid = req.query["fid"];
+    // console.log(fid);
+    // chat.to(room).emit(fid);
+    chat.to(room).emit("chat message", data.msg);
+  });
+});
+
+server.listen(3000, function () {
+  console.log("Socket IO server listening on port 3000");
+});
