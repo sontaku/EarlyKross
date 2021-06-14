4b6019f8ac79b1ead277048263e4431d3fb99665
dnrms47 <dnrms47@naver.com>
2021-06-14 10:14:41 +0900

    관리자페이지 형식 맞춤

--- src/main/java/com/ek/earlykross/controller/AdminController.java
+++ src/main/java/com/ek/earlykross/controller/AdminController.java
@@ -2,17 +2,13 @@ package com.ek.earlykross.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminController {
 
   @GetMapping("admin")
   public void AdminPage(){
-
-  }
-
-  @GetMapping("admin/admin_header")
-  public void AdminPageHeader(){
 
   }
 }
